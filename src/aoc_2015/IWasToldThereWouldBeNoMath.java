e7c1a9fb0aa9e912c78f296610b7a13216d87fe2
aniruth <ani5ruthn@gmail.com>
2025-03-23 16:56:57 +0530

    AOC 2015 - Day 2

---
+++ src/aoc_2015/IWasToldThereWouldBeNoMath.java
@@ -0,0 +1,52 @@
+package aoc_2015;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class IWasToldThereWouldBeNoMath {
+
+  record Dimensions(int l, int w, int h) {
+  }
+
+  Integer part1(List<Dimensions> dimensionsList) {
+    return dimensionsList.stream()
+      .map(dimensions -> {
+        int area1 = dimensions.l * dimensions.w;
+        int area2 = dimensions.l * dimensions.h;
+        int area3 = dimensions.w * dimensions.h;
+        int surfaceArea = 2 * area1 + 2 * area2 + 2 * area3;
+        int extra = Math.min(Math.min(area1, area2), area3);
+        return surfaceArea + extra;
+      })
+      .reduce(Integer::sum)
+      .orElseThrow(RuntimeException::new);
+  }
+
+  Integer part2(List<Dimensions> dimensionsList) {
+    return dimensionsList.stream()
+      .map(dimensions -> {
+        List<Integer> list = Stream.of(dimensions.l, dimensions.w, dimensions.h)
+          .sorted()
+          .limit(2)
+          .toList();
+        Integer smallest = list.get(0);
+        Integer secondSmallest = list.get(1);
+        return (smallest + smallest + secondSmallest + secondSmallest) + (dimensions.l * dimensions.w * dimensions.h);
+      })
+      .reduce(Integer::sum)
+      .orElseThrow(RuntimeException::new);
+  }
+
+  public static void main(String[] args) {
+    List<Dimensions> dimensionsList = Arrays.stream(args)
+      .map(arg -> {
+        String[] xes = arg.split("x");
+        return new Dimensions(Integer.parseInt(xes[0]), Integer.parseInt(xes[1]), Integer.parseInt(xes[2]));
+      })
+      .toList();
+    IWasToldThereWouldBeNoMath iWasToldThereWouldBeNoMath = new IWasToldThereWouldBeNoMath();
+    System.out.println(iWasToldThereWouldBeNoMath.part1(dimensionsList));
+    System.out.println(iWasToldThereWouldBeNoMath.part2(dimensionsList));
+  }
+}
